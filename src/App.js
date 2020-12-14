a6ab70bb247c0b013075e4271f7dd5048fd922f1
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-12-13 22:42:33 -0800

    Start kingdom game. Finish board cell highlight and switch logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+   stateOne: true,
+   stateTwo: false,
+   board: ['true', ''],
+   boardTwo: ['', '']
+  }
+
+  onClickHandler = (event, index) => {
+    let array = [...this.state.boardTwo];
+    let arrayTwo = new Array(this.state.board.length).fill("");
+    if (this.state.board[index] && !this.state.boardTwo[index]) {
+      array[index] = "true";
+      this.setState({ boardTwo: array });
+    } else if (this.state.board[index] && this.state.boardTwo[index]) {
+      array[index] = "";
+      this.setState({ boardTwo: array });
+    } else if (!this.state.board[index] && !this.state.boardTwo[index] && this.state.boardTwo.some(el => el)) {
+      arrayTwo[index] = true;
+      this.setState({ board: arrayTwo, boardTwo: ["", ""]});
+    }
+  }
+
+  render() {
+    return (
+     <div>
+        {this.state.board.map((el, index) => {
+          return (
+            <div key={index} className="Cell" onClick={(event) => this.onClickHandler(event, index)}>
+              { this.state.board[index] && this.state.boardTwo[index] && <div>Highlighted</div>} 
+              { this.state.board[index] && <div>Piece</div>}
+            </div>
+          )}
+        )};
+     </div>
+    );
+  }
 }
 
 export default App;
